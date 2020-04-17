eab673765250f3f534c07a5c9fe18c6847963840
Felipe Oliveira <felipe_96sp@msn.com>
2020-04-16 23:58:07 -0300

    Change App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 
-import api from "./services/api";
+import Home from "./pages/Home";
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function getDataApi() {
-      try {
-        const result = await api.get(
-          "https://brasil.io/api/dataset/covid19/caso/data?is_last=True&state=CE"
-        );
-
-        setData(result.data.results);
-      } catch (e) {
-        alert(e.response.message);
-      }
-    }
-
-    getDataApi();
-  }, []);
-
   return (
-    <div className='App'>
-      <div className='box-container'>
-        {data &&
-          data.map((d) => (
-            <div key={d.city_ibge_code} className='box'>
-              <p>
-                Cidade: <span>{d.city}</span>
-              </p>
-              <p>Confirmados: {d.confirmed}</p>
-              <p>Mortes: {d.deaths}</p>
-            </div>
-          ))}
-      </div>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
